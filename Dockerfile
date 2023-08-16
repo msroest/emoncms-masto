b182eb1006ac31b69553ccb1b146bc8a4ec8ba20
Mike Roest <msroest@gmail.com>
2023-08-15 21:58:59 -0600

    generate images

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM mcr.microsoft.com/dotnet/sdk:7.0 as build-env
-
+RUN apt-get update && apt-get install -y fontconfig libfontconfig1 libfreetype6
 WORKDIR /src
 COPY *.csproj .
 RUN dotnet restore
@@ -7,9 +7,10 @@ COPY * .
 RUN dotnet publish -c Release -o /publish
 
 FROM mcr.microsoft.com/dotnet/runtime:7.0 as runtime
-ENV DOTNET_RUNNING_IN_CONTAINER=true
+ENV DOTNET_RUNNING_IN_CONTAINER=true4
 ENV DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=0
 ENV CULTURE=en-us
 WORKDIR /publish
 COPY --from=build-env /publish .
+RUN apt-get update && apt-get install -y fontconfig libfontconfig1 libfreetype6
 ENTRYPOINT ["dotnet", "emoncms-masto.dll"]
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Mastonet;
 using System.Globalization;
+using ScottPlot;
 
 HttpClient client = new HttpClient();
 
@@ -8,7 +9,8 @@ var config = new ConfigurationBuilder()
 .AddJsonFile("appsettings.json", true)
 .AddEnvironmentVariables()
 .Build();
-if(!string.IsNullOrEmpty(config["CULTURE"])) {
+if (!string.IsNullOrEmpty(config["CULTURE"]))
+{
     CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(config["CULTURE"]!);
 }
 Uri? emonBaseUri = null;
@@ -24,12 +26,26 @@ if (string.IsNullOrEmpty(config["EMONCMS_READ_API_KEY"]))
     Environment.Exit(1);
 }
 
+if (string.IsNullOrEmpty(config["EMONCMS_SOLAR_KWH_FEED_ID"]) ||
+   string.IsNullOrEmpty(config["EMONCMS_USE_KWH_FEED_ID"]) ||
+   string.IsNullOrEmpty(config["EMONCMS_IMPORT_KWH_FEED_ID"]) ||
+   string.IsNullOrEmpty(config["EMONCMS_EXPORT_KWH_FEED_ID"]) ||
+   string.IsNullOrEmpty(config["EMONCMS_SOLAR_FEED_ID"]) ||
+   string.IsNullOrEmpty(config["EMONCMS_USE_FEED_ID"]))
+{
+    Console.Error.WriteLine("Missing required feed id");
+}
+
 var emonClient = new emoncmsmasto.EmonCMS(emonBaseUri, config["EMONCMS_READ_API_KEY"]!);
-var now = DateTime.Now;
-var solarFeed = await emonClient.GetDailyFeedData(int.Parse(config["EMONCMS_SOLAR_KWH_FEED_ID"]!), now.AddDays(-1), now);
-var useFeed = await emonClient.GetDailyFeedData(int.Parse(config["EMONCMS_USE_KWH_FEED_ID"]!), now.AddDays(-1), now);
-var importFeed = await emonClient.GetDailyFeedData(int.Parse(config["EMONCMS_IMPORT_KWH_FEED_ID"]!), now.AddDays(-1), now);
-var exportFeed = await emonClient.GetDailyFeedData(int.Parse(config["EMONCMS_EXPORT_KWH_FEED_ID"]!), now.AddDays(-1), now);
+var now = DateTime.Today;
+var yesterday = now.AddDays(-1);
+var solarFeed = await emonClient.GetDailyFeedData(int.Parse(config["EMONCMS_SOLAR_KWH_FEED_ID"]!), yesterday, now);
+var useFeed = await emonClient.GetDailyFeedData(int.Parse(config["EMONCMS_USE_KWH_FEED_ID"]!), yesterday, now);
+var importFeed = await emonClient.GetDailyFeedData(int.Parse(config["EMONCMS_IMPORT_KWH_FEED_ID"]!), yesterday, now);
+var exportFeed = await emonClient.GetDailyFeedData(int.Parse(config["EMONCMS_EXPORT_KWH_FEED_ID"]!), yesterday, now);
+var solarPowerFeed = await emonClient.GetFeedData(int.Parse(config["EMONCMS_SOLAR_FEED_ID"]!), yesterday, now.AddSeconds(-1));
+var usePowerFeed = await emonClient.GetFeedData(int.Parse(config["EMONCMS_USE_FEED_ID"]!), yesterday, now.AddSeconds(-1));
+
 var import = Math.Round(importFeed[1].reading - importFeed[0].reading, 2);
 var export = Math.Round(exportFeed[1].reading - exportFeed[0].reading, 2);
 var rate = double.Parse(config["RATE"]!);
@@ -54,6 +70,23 @@ rate.ToString("C3"),
 config["HASHTAGS"]
 );
 Console.Out.WriteLine($"Sending Message to Mastodon: {message}");
+var plt = new ScottPlot.Plot();
+var solarFill = plt.Add.FillY(solarPowerFeed.Select(x => x.dateTime.ToOADate()).ToArray(), solarPowerFeed.Select(x => x.reading).ToArray(), solarPowerFeed.Select(x => 0.0).ToArray());
+var useFill = plt.Add.FillY(usePowerFeed.Select(x => x.dateTime.ToOADate()).ToArray(), usePowerFeed.Select(x => x.reading).ToArray(), usePowerFeed.Select(x => 0.0).ToArray());
+
+useFill.FillStyle.Color = Colors.Blue.WithAlpha(175);
+solarFill.FillStyle.Color = Colors.Gold;
+plt.AxisStyler.DateTimeTicks(Edge.Bottom);
+plt.Style.Background(figure: new Color(51, 51, 51), data: new Color(51, 51, 51));
+plt.Style.ColorAxes(Color.FromHex("#FFFFFF"));
+plt.Style.ColorGrids(Color.FromHex("#FFFFFF"));
+plt.YAxis.Label.Text = "Watts";
+
+plt.Title($"Solar Generation for {yesterday.ToString("yyyy/MM/dd")}");
+plt.AutoScale();
+plt.Margins(0, 0.05);
+plt.SavePng("./output/generation.png", 1920, 1080);
+
 var mastoClient = new MastodonClient(config["MASTODON_URI"]!, config["MASTODON_API_KEY"]!, client);
 
 var publish = true;
@@ -66,5 +99,10 @@ if (!string.IsNullOrEmpty(config["PUBLISH"]))
 }
 if (publish)
 {
-    await mastoClient.PublishStatus(message, Visibility.Public);
+    using (var fs = File.OpenRead("./output/generation.png"))
+    {
+        var media = await mastoClient.UploadMedia(fs, $"{yesterday.ToString("yyyy-MM-dd")}.png",
+        $"Graph of Solar generation and home power usage of a home in Calgary, Alberta, Canada for {yesterday.ToString("yyyy-MM-dd")}");
+        await mastoClient.PublishStatus(message, visibility: Visibility.Private, mediaIds: new[] { media.Id });
+    }
 }
